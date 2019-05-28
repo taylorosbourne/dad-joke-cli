e2f95b03a3917f565b2b70254799b31e26de443d
Taylor-Osbourne <Taylor.osbourne@coxautoinc.com>
2019-05-28 10:06:58 -0400

    trying to clean up travis errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 
 const API = 'https://icanhazdadjoke.com/'
 
-getDadJoke = async () => {
+const getDadJoke = async () => {
   const res = await fetch(API, {
     headers: {
       Accept: 'application/json'
@@ -18,8 +18,8 @@ getDadJoke = async () => {
 
 class DadJokeCommand extends Command {
   async run() {
-    const {flags} = this.parse(DadJokeCommand)
-    const name = flags.name || 'world'
+    // const {flags} = this.parse(DadJokeCommand)
+    // const name = flags.name || 'world'
     // this.log(`Dad Joke here`)
     getDadJoke()
   }
