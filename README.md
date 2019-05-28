64dc2b68c76c3f75832af05ae3e71ce7870ebec3
Taylor-Osbourne <Taylor.osbourne@coxautoinc.com>
2019-05-28 09:09:56 -0400

    returning dad joke from api

---
+++ README.md
@@ -0,0 +1,15 @@
+dad-joke
+========
+
+You want the dad jokes, this brings the dad jokes
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/dad-joke.svg)](https://npmjs.org/package/dad-joke)
+[![Downloads/week](https://img.shields.io/npm/dw/dad-joke.svg)](https://npmjs.org/package/dad-joke)
+[![License](https://img.shields.io/npm/l/dad-joke.svg)](https://github.com/taylorosbourne/dad-joke/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.js
@@ -0,0 +1,41 @@
+const {Command, flags} = require('@oclif/command')
+const fetch = require('node-fetch')
+
+
+const API = 'https://icanhazdadjoke.com/'
+
+getDadJoke = async () => {
+  const res = await fetch(API, {
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+  const  data = await res.json()
+  console.log(data.joke)
+}
+
+
+
+class DadJokeCommand extends Command {
+  async run() {
+    const {flags} = this.parse(DadJokeCommand)
+    const name = flags.name || 'world'
+    // this.log(`Dad Joke here`)
+    getDadJoke()
+  }
+}
+
+DadJokeCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`
+
+DadJokeCommand.flags = {
+  // add --version flag to show CLI version
+  version: flags.version({char: 'v'}),
+  // add --help flag to show CLI version
+  help: flags.help({char: 'h'}),
+  name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = DadJokeCommand
